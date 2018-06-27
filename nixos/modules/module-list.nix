554add8bab01313df56a2b2d3ed5259e8452444b
Richard Marko <srk@48.io>
2018-06-28 01:55:49 +0200

    ttn initial

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -544,6 +544,7 @@
   ./services/networking/openvpn.nix
   ./services/networking/ostinato.nix
   ./services/networking/owamp.nix
+  ./services/networking/packet_forwarder.nix
   ./services/networking/pdnsd.nix
   ./services/networking/polipo.nix
   ./services/networking/powerdns.nix
