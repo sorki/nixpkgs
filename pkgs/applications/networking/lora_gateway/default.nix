a0b5a8a19eebbaad71d2fabe4d7449f50c9fd140
Richard Marko <srk@48.io>
2018-07-04 03:16:22 +0200

    loragw from master

--- pkgs/applications/networking/lora_gateway/default.nix
+++ pkgs/applications/networking/lora_gateway/default.nix
@@ -6,19 +6,20 @@ with stdenv.lib;
 
 stdenv.mkDerivation rec {
   name = "lora_gateway-${version}";
-  version = "5.0.1";
+  version = "4.0.0";
 
   src = fetchFromGitHub {
     owner = "TheThingsNetwork";
     repo = "lora_gateway";
-    rev = "f4329a63a08fb643d1e05815529f8be1461b7dd9";
-    sha256 = "0i5ayvw7gzczf6pdih4xlqxi4n8dl7s0fjssjfni181x6jd3y5s8";
+    rev = "4e2d95646e3a34224b460e69b6b417796b83b4ce";
+    sha256 = "0sgwis6izgcc3xdq8b8ghazasi47s4i4hwj3hxj54fn82ppbw1sc";
   };
 
   outputs = [ "out" "dev" ];
 
   preConfigure = ''
-    sed -i 's~PLATFORM= kerlink~PLATFORM= ${gwPlatform}~' libloragw/library.cfg
+    sed -i 's~PLATFORM= .*~PLATFORM= ${gwPlatform}~' libloragw/library.cfg
+    sed -i 's~CFG_SPI= .*~CFG_SPI= native~' libloragw/library.cfg
     sed -i 's~/dev/spidev1.0~/dev/spidev0.0~' libloragw/inc/lorank.h
   '';
 
